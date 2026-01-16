5ee260bfdb06845766b539bf3958d536c776aec0
Turazzi <analivia.turazzi@gmail.com>
2026-01-15 21:44:37 -0300

    feat: adds blood bag status enum

--- demo/src/main/java/enums/BloodBagStatus.java
+++ demo/src/main/java/enums/BloodBagStatus.java
@@ -1,4 +1,27 @@
 package enums;
 
 public enum BloodBagStatus {
+
+    IN_ANALYSIS("Em análise laboratorial", false),
+    AVAILABLE("Disponível para uso", true),
+    RESERVED("Reservada para paciente", false),
+    TRANSFUSED("Já utilizada (Baixa no estoque)", false),
+    DISCARDED("Descartada (Validade ou Imprópria)", false);
+
+    private final String description;
+    private final boolean availableForTransfusion;
+
+    BloodBagStatus(String description, boolean availableForTransfusion) {
+        this.description = description;
+        this.availableForTransfusion = availableForTransfusion;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isAvailableForTransfusion() {
+        return availableForTransfusion;
+    }
+
 }
