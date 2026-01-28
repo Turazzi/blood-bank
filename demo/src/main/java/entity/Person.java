cabe1e996f571191b82ebe8a14a87734840679d5
Turazzi <analivia.turazzi@gmail.com>
2026-01-27 23:08:57 -0300

    feat: Adds gender and persons classes

--- demo/src/main/java/entity/Person.java
+++ demo/src/main/java/entity/Person.java
@@ -1,4 +1,94 @@
 package entity;
 
+import enums.BloodType;
+import enums.Gender;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String rg;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BloodType bloodType;
+
+    @Column(nullable = false)
+    private String mothersName;
+
+    public Person() {}
+
+    public Person(String name, String rg, String cpf, String email, Gender gender, String phone, LocalDate birthDate, BloodType bloodType, String mothersName) {
+        this.name = name;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.bloodType = bloodType;
+        this.mothersName = mothersName;
+    }
+
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getRg() { return rg; }
+    public void setRg(String rg) { this.rg = rg; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public Gender getGender() { return gender; }
+    public void setGender(Gender gender) { this.gender = gender; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public LocalDate getBirthDate() { return birthDate; }
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+    public BloodType getBloodType() { return bloodType; }
+    public void setBloodType(BloodType bloodType) { this.bloodType = bloodType; }
+    public String getMothersName() { return mothersName; }
+    public void setMothersName(String mothersName) { this.mothersName = mothersName; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(cpf, person.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
 }
--- demo/src/main/java/enums/Gender.java
+++ demo/src/main/java/enums/Gender.java
@@ -1,4 +1,20 @@
 package enums;
 
 public enum Gender {
+    MALE("Masculino", 'M', 60),
+    FEMALE("Feminino", 'F', 90);
+
+    private final String description;
+    private final char code;
+    private final int minDaysBetweenDonations;
+
+    Gender(String description, char code, int minDaysBetweenDonations) {
+        this.description = description;
+        this.code = code;
+        this.minDaysBetweenDonations = minDaysBetweenDonations;
+    }
+
+    public String getDescription() { return description; }
+    public char getCode() { return code; }
+    public int getMinDaysBetweenDonations() { return minDaysBetweenDonations; }
 }
